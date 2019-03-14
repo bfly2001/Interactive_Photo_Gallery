e7f7dae3cf1e51adb56bd340fd492aa1550cee95
bfly2001 <brendan.sflynn@gmail.com>
2019-03-14 14:04:07 -0700

    Added captions to photos and started on js search functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,1 @@
 
-/*function myFunction() {
-    var input,filter,ul,li, a,i, txtValue;
-    input = document.getElementById("photosearch");
-    filter = input.value.toLowerCase();
-    .inner-wrapper = document.getElementById("photosearch");
-    img = img.getElementByTagName("a");
-    for(i = 0; i < img.length; i++) {
-      a = img[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          a[i].style.display = "";
-        else {
-          a[i].style.display = "none";
-        }
-      }
-    }*/
